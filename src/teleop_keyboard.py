11eb770076758caaf7f55fa9c171a0a5a241251c
intellicode-demo <57885966+intellicode-demo@users.noreply.github.com>
2019-11-21 09:24:54 +0900

    Update teleop_keyboard.py

--- src/teleop_keyboard.py
+++ src/teleop_keyboard.py
@@ -50,7 +50,7 @@ ANG_VEL_STEP_SIZE = 0.1
 TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
 
 msg = """
-Control Your TurtleBot3!!
+Control Your TurtleBot3 source!!
 ---------------------------
 Moving around:
         w
@@ -80,7 +80,7 @@ def get_key(settings):
 
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
     '''
-    TEST_KEY = 'a'
+    TEST_KEY = 'd'
     time.sleep(10)
     return TEST_KEY
 
